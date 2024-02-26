471d2d89859d6d41c22188a0556cf5643e90712f
Christoph Welcz <christoph.welcz@codecentric.de>
2024-02-26 10:36:19 +0100

    🤡 mock api for tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,36 @@
-import {AppComponent} from './app.component';
-import {render, screen, within} from "@testing-library/angular";
+import { AppComponent } from './app.component'
+import { render, screen, within } from '@testing-library/angular'
+import { Todo } from './types'
+import { Observable, of } from 'rxjs'
+import { ApiService } from './api.service'
 
 describe('AppComponent', () => {
+  const todos: Todo[] = [
+    { completed: false, id: 0, title: 'First', userId: 0 },
+    { completed: false, id: 1, title: 'Second', userId: 0 },
+    { completed: true, id: 2, title: 'Third', userId: 0 },
+  ]
+
   it('renders component', async () => {
-    await render(AppComponent)
+    fetchComments.mockReturnValue(of(todos))
+
+    await renderComponent()
 
     const title = screen.getByRole('heading')
     expect(title).toHaveTextContent('Hello, Todos!')
     const list = await screen.findByRole('list')
-    const items = await within(list).findAllByRole('listitem');
-    expect(items).not.toHaveLength(0)
-  });
-});
+    const items = await within(list).findAllByRole('listitem')
+    expect(items).toHaveLength(todos.length)
+    expect(items[0]).toHaveTextContent('First')
+    expect(items[1]).toHaveTextContent('Second')
+    expect(items[2]).toHaveTextContent('Third')
+  })
+})
+
+const fetchComments = jest.fn<Observable<Todo[]>, any>()
+
+function renderComponent() {
+  return render(AppComponent, {
+    providers: [{ provide: ApiService, useValue: { fetchComments } }],
+  })
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,22 @@
 import { Component, inject } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { AsyncPipe } from '@angular/common'
 import { injectQuery } from '@ngneat/query'
-
-type Todo = {
-  userId: number
-  id: number
-  title: string
-  completed: boolean
-}
+import { ApiService } from './api.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Todos'
-  #http = inject(HttpClient)
+  #api = inject(ApiService)
   #query = injectQuery()
   todosQuery$ = this.#query({
     queryKey: ['todos'],
-    queryFn: () => this.fetchComments(),
+    queryFn: () => this.#api.fetchComments(),
   }).result$
-
-  private fetchComments() {
-    return this.#http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-  }
 }
